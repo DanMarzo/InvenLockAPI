b27b7c1752d76e96fba01783c1386a316b63deca
DanMarzo <marzogildan@gmail.com>
2022-11-27 19:33:43 -0300

    Metodo de validar equipamento existente e metodo post

--- InvenLock/Controllers/EquipamentoController.cs
+++ InvenLock/Controllers/EquipamentoController.cs
@@ -1,6 +1,9 @@
 ﻿using InvenLock.Data;
+using InvenLock.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace InvenLock.Controllers
 {
@@ -12,6 +15,51 @@ namespace InvenLock.Controllers
         public EquipamentoController(DataContext context)
         {
             _context = context;
+        }
+
+        public bool ValidaUnicidade(Equipamento validar)
+        {
+            Equipamento confirm = _context.Equipamentos.FirstOrDefaultAsync(x => x.Codigo == validar.Codigo);
+            if (confirm != null)
+                return true;
+            else
+                return false;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                List<Equipamento> ListaEquip = await _context.Equipamentos.ToListAsync();
+                if(ListaEquip == null)
+                    return Ok(ListaEquip);
+                else
+                    throw new Exception("Nenhum equipamento encontrado, tente adicionar equipamento ");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("NovoEquip")]
+        public async Task<IActionResult> AddNovoEquipAsync(Equipamento novo)
+        {
+            try
+            {
+                if(confirm == null)
+                {
+                    _context.Equipamentos.Add(novo);
+                }
+                else
+                {
+                    throw new Exception("");
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
         }
     }
 }
