371fbda2fe64b54efeafb51b707937e2c30dd775
DanMarzo <marzogildan@gmail.com>
2022-11-27 19:32:59 -0300

    Consertando espaco de coluna

---
+++ InvenLock/Migrations/20221127170426_Equipamentos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvenLock.Migrations
+{
+    /// <inheritdoc />
+    public partial class Equipamentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Equipamentos",
+                columns: table => new
+                {
+                    Codigo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeEquip = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fabricante = table.Column<int>(type: "int", nullable: false),
+                    TipoEquipamento = table.Column<int>(type: "int", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SituacaoEquip = table.Column<int>(type: "int", nullable: false),
+                    DataCompra = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataFimEquip = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipamentos", x => x.Codigo);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Equipamentos");
+        }
+    }
+}
