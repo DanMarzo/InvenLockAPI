771106f4de81378e7cd0bc9f1abd964c00acdfa7
DanMarzo <marzogildan@gmail.com>
2022-11-27 13:46:11 -0300

    Estabelecendo contexto do banco de dados

---
+++ InvenLock/Controllers/EquipamentoController.cs
@@ -0,0 +1,17 @@
+﻿using InvenLock.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvenLock.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EquipamentoController : ControllerBase
+    {
+        public readonly DataContext _context;
+        public EquipamentoController(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
