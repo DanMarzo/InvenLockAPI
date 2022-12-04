58a404a4f86ca73c7dd09533fe05b94ad469abfd
DanMarzo <marzogildan@gmail.com>
2022-12-03 21:05:11 -0300

    EquipEmprestimo criado, funções pendentes

---
+++ InvenLock/Models/EquipEmprestimo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvenLock.Models
+{
+    public class EquipEmprestimo
+    {
+        [Key]
+        [Required]
+        public int                IdEquipEmprestimo { get; set; }
+        public List<EstoqueEquip> EstoqueEquip      { get; set; }
+        public int                IdFunc            { get; set; }
+        public Funcionario        Funcionario       { get; set; }
+        public int                IdForm            { get; set; }
+        public FormEmprestimo     FormEmprestimo    { set; get; }
+        public DateTime           DataEmprestimo    { get; set; }
+        public DateTime?          DataDevolucao     { get; set; }
+
+    }
+}
