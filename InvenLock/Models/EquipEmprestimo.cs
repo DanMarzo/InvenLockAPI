cf1386462b7abe60295cfd5c6726ccf0bd93fcd6
DanMarzo <marzogildan@gmail.com>
2022-12-03 22:08:26 -0300

    Feita a adicao do IdFunc em EquipEmprestimo e formEmprestimo, afim de garantir que cada um tenha somente uma ligação

--- InvenLock/Models/EquipEmprestimo.cs
+++ InvenLock/Models/EquipEmprestimo.cs
@@ -8,6 +8,8 @@ namespace InvenLock.Models
         [Required]
         public int                IdEquipEmprestimo { get; set; }
         public List<EstoqueEquip> EstoqueEquip      { get; set; }
+        public int                IdFunc            { get; set; }
+        public Funcionario        Funcionario       { get; set; }
         public int                IdForm            { get; set; }
         public FormEmprestimo     FormEmprestimo    { set; get; }
         public DateTime           DataEmprestimo    { get; set; }
