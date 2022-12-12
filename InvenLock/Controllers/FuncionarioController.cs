1707cca2e576a96c04627069ebf1f54655939166
DanMarzo <marzogildan@gmail.com>
2022-12-11 22:23:26 -0300

    FuncionarioController criado => NovoFuncionario criado

--- InvenLock/Controllers/FuncionarioController.cs
+++ InvenLock/Controllers/FuncionarioController.cs
@@ -1,6 +1,41 @@
-﻿namespace InvenLock.Controllers
+﻿using InvenLock.Data;
+using InvenLock.Models;
+using InvenLock.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvenLock.Controllers
 {
-    public class FuncionarioController
+    [ApiController]
+    [Route("[controller]")]
+    public class FuncionarioController : ControllerBase
     {
+        public readonly DataContext _context;
+        public FuncionarioController(DataContext context){ _context = context; }
+        
+        [HttpPost]
+        public async Task<IActionResult> NovoFuncionario(Funcionario funcionario)
+        {
+            try
+            {
+                Funcionario verificar = await _context.Funcionarios.FirstOrDefaultAsync(x => x.Cpf == funcionario.Cpf);
+                if (verificar != null)
+                    throw new Exception("O CPF já esta em uso, por favor verificar a lista de funcionarios");
+                
+                Criptografia.CriarPasswordHash(funcionario.PasswordString, out byte[] hash, out byte[] salt);
+                funcionario.PasswordSalt = salt;
+                funcionario.PasswordHash = hash;
+
+                funcionario.Admissao = DateTime.Now;
+
+                await _context.Funcionarios.AddAsync(funcionario);
+                await _context.SaveChangesAsync();
+                return Ok($"Funcionario {funcionario.Nome} salvo com sucesso!");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
